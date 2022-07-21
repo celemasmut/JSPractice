d16b8d23ef798b805daf8c03b1bf0a5300a8fab5
Celeste Masmut <celeste.masmut.ext@pedidosya.com>
2022-07-21 00:28:40 -0300

    fetch DolarBlue

--- budgetApp/js/app.js
+++ budgetApp/js/app.js
@@ -186,12 +186,13 @@ const agregarDato = ()=> {
 }
 const API_URL = 'https://api.bluelytics.com.ar/';
 const endPointDolarBlue = 'v2/latest';
-
+let dolarBlue = [];
 const obtenerDatosDolar = () => {
     fetch(API_URL + endPointDolarBlue)
     .then((response) => response.json())
     .then((responseData) => {
-        console.log(responseData)
+        console.log(responseData);
+        dolarBlue = responseData.blue;
     });
 }
 obtenerDatosDolar();  
